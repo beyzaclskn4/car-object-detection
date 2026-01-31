6e48b402f71faeab911cb6fa5589f49cda99df2b
beyzaclskn4 <beyza.caliskan4006@gmail.com>
2026-01-31 16:22:26 +0300

    feat: add __len__ and __getitem__ for detection dataset

--- src/dataset.py
+++ src/dataset.py
@@ -12,3 +12,26 @@ class CarDataset(Dataset):
         self.transform = transform
 
         self.image_ids = self.labels["image"].unique()
+
+    def __len__(self):
+        return len(self.image_ids)
+
+    def __getitem__(self, idx):
+        image_id = self.image_ids[idx]
+        image_path = os.path.join(self.images_dir, image_id)
+
+        image = Image.open(image_path).convert("RGB")
+
+        records = self.labels[self.labels["image"] == image_id]
+
+        boxes = records[["xmin", "ymin", "xmax", "ymax"]].values
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        target = {
+            "boxes": boxes
+        }
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image, target
