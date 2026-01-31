3e4be9be567ea012f5ccc8404e27f2322bc8cf69
beyzaclskn4 <beyza.caliskan4006@gmail.com>
2026-01-31 16:52:08 +0300

    setup train and validation dataloaders

--- src/train.py
+++ src/train.py
@@ -29,3 +29,39 @@ LR = 1e-4
 
 MODEL_SAVE_PATH = "experiments/best_model.pth"
 os.makedirs("experiments", exist_ok=True)
+
+# ========== TRANSFORMS ==========
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+# ========== DATASETS & DATALOADERS ==========
+# ========== DATASET & DATALOADER ==========
+train_dataset = CarDataset(
+    csv_path=TRAIN_CSV,
+    img_dir=TRAIN_IMG_DIR,
+    transform=transform
+)
+
+val_dataset = CarDataset(
+    csv_path=VAL_CSV,
+    img_dir=VAL_IMG_DIR,
+    transform=transform
+)
+
+train_loader = DataLoader(
+    train_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=True
+)
+
+val_loader = DataLoader(
+    val_dataset,
+    batch_size=BATCH_SIZE,
+    shuffle=False
+)
+
