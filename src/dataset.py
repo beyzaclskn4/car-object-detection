8c50e96a58b9b2766e584a891cde4e88f2ada8d3
beyzaclskn4 <beyza.caliskan4006@gmail.com>
2026-01-31 16:21:27 +0300

    feat: implement CarDataset initializer

--- src/dataset.py
+++ src/dataset.py
@@ -4,3 +4,11 @@ from PIL import Image #görsel okuma için
 
 import torch
 from torch.utils.data import Dataset
+
+class CarDataset(Dataset):
+    def __init__(self, images_dir, labels_csv, transform=None):
+        self.images_dir = images_dir
+        self.labels = pd.read_csv(labels_csv)
+        self.transform = transform
+
+        self.image_ids = self.labels["image"].unique()
